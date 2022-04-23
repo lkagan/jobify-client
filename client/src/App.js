a6723f5d8c790b189d8a64a9c907afe879a5030c
Larry Kagan <larry@superiocity.com>
2022-04-22 22:25:16 -0400

    Intro to styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import Landing from "./pages/Landing";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 1rem;
+`;
+
+const ButtonSecond = styled.button`
+  background: blue;
+  color: white;
+  font-size: 1rem;
+`;
 
 function App() {
-  return (
-      <>
-      <h1>Jobify</h1>
-        <Landing />
-      </>
-  );
+    return (
+        <>
+            <Button>Click Me</Button>
+            <ButtonSecond>Click Me</ButtonSecond>
+            <h1>Jobify</h1>
+            <Landing/>
+        </>
+    );
 }
 
 export default App;
