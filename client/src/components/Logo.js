b062e992ae9cd3d6016801a42fb95ef7810ed5f1
Larry Kagan <larry@superiocity.com>
2022-04-23 11:01:08 -0400

    Extract Logo and styled components to their own files

---
+++ client/src/components/Logo.js
@@ -0,0 +1,14 @@
+import logo from "../assets/images/logo.svg";
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <img
+            src={ logo }
+            alt="Jobify"
+            className={ "logo" }
+        />
+    );
+}
+
+export default Logo;
