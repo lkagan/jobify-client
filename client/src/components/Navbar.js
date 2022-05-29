2e4f2b2ec44d0703713a550343ddaf1cf3a72834
Larry Kagan <larry@superiocity.com>
2022-05-29 14:48:46 -0400

    Complete navbar structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaBars, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from '../context/appContext';
+import Logo from "./Logo";
 
 const Navbar = () => {
     return (
         <Wrapper>
-            Navbar
-            <FaHome />
+            <div className="nav-center">
+                <button
+                    type="button"
+                    className="toggle-btn"
+                    onClick={ () => console.log('toggle sidebar') }
+                >
+                    <FaBars/>
+                </button>
+                <div>
+                    <Logo/>
+                    <h3 className="logo-text">
+                        dashboard
+                    </h3>
+                </div>
+                <div className="btn-container">
+                    <button
+                        type="button"
+                        className="btn"
+                        onClick={ () => console.log('toggle dropdown') }
+                    >
+                        <FaUserCircle/>
+                        John
+                        <FaCaretDown/>
+                    </button>
+                    <div className="dropdown show-dropdown">
+                        <button
+                            type="button"
+                            className="dropdown-btn"
+                            onClick={ () => console.log('logout user')}
+                        >logout
+                        </button>
+                    </div>
+                </div>
+            </div>
         </Wrapper>
     );
 };
