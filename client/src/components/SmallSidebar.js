7629c7d06217192f5545b561874066850c2f3dbc
Larry Kagan <larry@superiocity.com>
2022-06-04 11:59:00 -0400

    Display links in mobile navbar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -5,28 +5,47 @@ import { useAppContext } from "../context/appContext";
 import links from '../utils/links';
 import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
+import { TOGGLE_SIDEBAR } from '../context/actions';
 
 const SmallSidebar = () => {
     const { showSidebar, toggleSidebar } = useAppContext();
 
     return (
-      <Wrapper>
-        <div
-          className={
-            showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
-          }
-        >
-          <div className="content">
-            <button className="close-btn" onClick={toggleSidebar}>
-              <FaTimes />
-            </button>
-            <header>
-              <Logo />
-            </header>
-            <div className="nav-links">nav links</div>
-          </div>
-        </div>
-      </Wrapper>
+        <Wrapper>
+            <div
+                className={
+                    showSidebar
+                        ? "sidebar-container show-sidebar"
+                        : "sidebar-container"
+                }
+            >
+                <div className="content">
+                    <button className="close-btn" onClick={toggleSidebar}>
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className="nav-links">
+                        {links.map((link) => {
+                            const { text, path, id, icon } = link;
+
+                            return (
+                                <NavLink
+                                    to={path}
+                                    key={id}
+                                    onClick={TOGGLE_SIDEBAR}
+                                    className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
+                                >
+                                    <span className="icon">{icon}</span>
+                                    {text}
+                                </NavLink>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
     );
 };
 
