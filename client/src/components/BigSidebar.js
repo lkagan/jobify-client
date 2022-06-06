56e355f028b055ec276b66e22ec6d3852bf04abf
Larry Kagan <larry@superiocity.com>
2022-06-06 11:47:15 -0400

    Setup sidebar for large screens

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
+    const { showSideBar, toggleSidebar } = useAppContext();
+
     return (
         <Wrapper>
-            Big Sidebar
+            <div
+                className={
+                    showSideBar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+                }
+            >
+                <div className="content">
+                    <header>
+                        <Logo />
+                    </header>
+                    <NavLinks toggleSidebar={ toggleSidebar }/>
+                </div>
+            </div>
         </Wrapper>
     );
 };
