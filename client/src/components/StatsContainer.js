7b5e3a71aebdeea4e8b007b5d99b8ccef36a4ff0
Larry Kagan <larry@superiocity.com>
2022-06-30 12:55:11 -0400

    Show stats in component

--- client/src/components/StatsContainer.js
+++ client/src/components/StatsContainer.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import StatsItem from "./StatsItem";
+import { useAppContext } from "../context/appContext";
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from "react-icons/fa";
+import Wrapper from '../assets/wrappers/StatsContainer';
 
 const StatsContainer = () => {
+    const { stats } = useAppContext();
+
+    const statsItemData = [
+        {
+            title: 'pending applications',
+            count: stats.pending,
+            icon: <FaSuitcaseRolling/>,
+            color: '#e9b949',
+            bgc: '#fcefc7',
+        },
+        {
+            title: 'interviews schedule',
+            count: stats.interview,
+            icon: <FaCalendarCheck/>,
+            color: '#647acb',
+            bgc: '#e0e8f9',
+        },
+        {
+            title: 'jobs declined',
+            count: stats.declined,
+            icon: <FaBug/>,
+            color: '#d66a6a',
+            bgc: '#ffeeee',
+        },
+    ];
+
     return (
-        <div>
-            <h1>Stats Container</h1>
-            <StatsItem />
-        </div>
+        <Wrapper>
+            {
+                statsItemData.map((item, index) => {
+                    return <StatsItem key={index} {...item} />
+                })
+            }
+        </Wrapper>
     );
 };
 
