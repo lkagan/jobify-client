e311469acea3ee4f9edd9c4d200e9748a8b12c20
Larry Kagan <larry@superiocity.com>
2022-07-02 11:12:50 -0400

    Area chart for jobs stats

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import {
+    ResponsiveContainer,
+    AreaChart as ReChartAreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+} from "recharts";
 
-const AreaChart = () => {
+const AreaChart = ({ data }) => {
     return (
-        <div>
-            <h2>
-                Area Chart
-            </h2>
-        </div>
+        <ResponsiveContainer
+            width='100%'
+            height={ 300 }
+        >
+            <ReChartAreaChart
+                data={ data }
+                margin={ { top: 50 } }
+            >
+                <CartesianGrid strokeDasharray='3 3'/>
+                <XAxis dataKey='date'/>
+                <YAxis allowDecimals={ false }/>
+                <Tooltip/>
+                <Area
+                    type='monotone'
+                    dataKey='count'
+                    stroke='#2cb1bc'
+                    fill='#bef8fd'
+                />
+            </ReChartAreaChart>
+        </ResponsiveContainer>
     );
 };
 
